da1f9927fe620de39f96c41f72e0ad8cdbbcede2
Kevin <kevin@rocher-dev.com>
2022-10-14 18:24:35 +0200

    v 0.0.2
    - Local storage.
    - Delay.
    - Independant tasks.
    - Skin fix.

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ function DkGantt() {
 
     function init() {
         build_editor();
-        editor_add_line();
+        editor_insert_initial_lines();
         build_tasks_lines();
         build_header_dates();
     }
@@ -64,11 +64,39 @@ function DkGantt() {
         }, 200);
     }
 
-    function editor_add_line() {
+    function editor_insert_initial_lines() {
+        var _items = JSON.parse(localStorage.getItem('dkgantt_items'));
+        if (_items) {
+            for (var i = 0, len = _items.length; i < len; i++) {
+                editor_add_line(_items[i]);
+            }
+        }
+        else {
+            editor_add_line();
+        }
+    }
+
+    function editor_add_line(_initialValues) {
+        _initialValues = _initialValues ? _initialValues : {};
+        _initialValues.randnum = Date.now();
+        if (!_initialValues.task_name) {
+            _initialValues.task_name = '';
+        }
+        if (!_initialValues.task_duration) {
+            _initialValues.task_duration = 5;
+        }
+        if (!_initialValues.task_delay) {
+            _initialValues.task_delay = 0;
+        }
+        if (!_initialValues.task_restart) {
+            _initialValues.task_restart = '';
+        }
+        else {
+            _initialValues.task_restart = 'checked';
+        }
+
         var li = document.createElement('li');
-        li.innerHTML = get_template('editor_line', {
-            randnum: Date.now()
-        });
+        li.innerHTML = get_template('editor_line', _initialValues);
         $editor.appendChild(li);
         setTimeout(function() {
             li.querySelector('[name="task_name"]').focus();
@@ -81,10 +109,24 @@ function DkGantt() {
     function build_tasks_lines() {
         var tmp_html = '',
             days = 0;
+
+        var _localStorageData = [];
         Array.prototype.forEach.call($editor.querySelectorAll('.gantt-editor__line'), function(el) {
-            var _nb_days_tmp = parseInt(el.querySelector('[name="task_duration"]').value, 10),
+            var _task_duration = el.querySelector('[name="task_duration"]').value,
+                _nb_days_tmp = parseInt(_task_duration, 10),
                 _nb_days = _nb_days_tmp,
+                _task_name = el.querySelector('[name="task_name"]').value,
+                _nb_delay_tmp = parseInt(el.querySelector('[name="task_delay"]').value, 10),
+                _nb_delay = _nb_delay_tmp,
+                _is_restart = el.querySelector('[name="task_restart"]').checked,
                 _start_day = days;
+
+            if (_is_restart) {
+                days = 0;
+                _start_day = 0;
+            }
+
+            _start_day += _nb_delay;
 
             /* Start date */
             var tmp_day = new Date(start_day);
@@ -100,7 +142,6 @@ function DkGantt() {
 
             /* Duration */
             for (var i = 0; i < _nb_days_tmp; i++) {
-                console.log(_nb_days_tmp);
                 if (tmp_day.getDay() === 0 || tmp_day.getDay() === 6) {
                     _nb_days += 1;
                     _nb_days_tmp++;
@@ -112,7 +153,14 @@ function DkGantt() {
             tmp_html += get_template('line', {
                 dayfromstart: _start_day,
                 dayduration: _nb_days,
-                task_name: el.querySelector('[name="task_name"]').value
+                task_name: _task_name
+            });
+
+            _localStorageData.push({
+                task_name: _task_name,
+                task_duration: _task_duration,
+                task_delay: _nb_delay_tmp,
+                task_restart: _is_restart
             });
 
             /* Prepare next task */
@@ -121,6 +169,8 @@ function DkGantt() {
         });
         nb_days_visible = days;
         $lines.innerHTML = tmp_html;
+
+        localStorage.setItem('dkgantt_items', JSON.stringify(_localStorageData));
 
     }
 
@@ -132,7 +182,7 @@ function DkGantt() {
         var tmp_day,
             tmp_date_day,
             tmp_html = '';
-            nb_days_visible = Math.max(min_days_visible,nb_days_visible)
+        nb_days_visible = Math.max(min_days_visible, nb_days_visible)
         for (var _d = 0; _d < nb_days_visible; _d++) {
             tmp_day = new Date(start_day);
             tmp_day.setDate(start_day.getDate() + _d);
@@ -145,7 +195,7 @@ function DkGantt() {
                 }),
                 dayname: tmp_day.toLocaleDateString(undefined, {
                     weekday: 'short'
-                }).substring(0,1),
+                }).substring(0, 1),
                 daynum: tmp_day.getDate()
             });
 
